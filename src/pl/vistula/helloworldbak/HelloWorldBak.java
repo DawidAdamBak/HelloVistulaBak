f5fbfe8c602efc2116f174a8048dea60e753b55f
DawidAdamBak <dbak1@stu.vistula.edu.pl>
2022-10-13 20:52:35 +0200

    Commit 1

--- src/pl/vistula/helloworldbak/HelloWorldBak.java
+++ src/pl/vistula/helloworldbak/HelloWorldBak.java
@@ -3,5 +3,7 @@ package pl.vistula.helloworldbak;
 public class HelloWorldBak {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Bak");
+        System.out.println("Hello Vistula Bak");
+        System.out.println("Hello Vistula Bak");
     }
 }
